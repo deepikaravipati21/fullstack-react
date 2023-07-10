1bf6b1bf63e23828f75acc71d7bb33e68833f9df
Deepika Ravipati <deepikaravipati@gmail.com>
2023-07-10 13:39:27 -0700

    committing front end changes removing submodule

---
+++ client/src/scenes/layout/index.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { Box, useMediaQuery } from "@mui/material"; //Box allows us ot pass in properties as CSS properties
+import { Outlet } from "react-router-dom"; // Allows us to have template layouts
+import { useSelector } from "react-redux";
+import Navbar from "components/Navbar";
+import Sidebar from "components/Sidebar";
+import { useGetUserQuery, userGetUserQuery } from "state/api";
+
+const Layout = () => {
+  const isNonMobile = useMediaQuery("(min-width: 600px)");
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const userId = useSelector((state) => state.global.userId);
+  const { data } = useGetUserQuery(userId);
+
+  return (
+  <Box display={isNonMobile ? "flex" : "block"} width="100%" height="100%">
+    <Sidebar
+      user = {data || {}}
+      isNonMobile={isNonMobile}
+      drawerWidth="250px"
+      isSidebarOpen={isSidebarOpen}
+      setIsSidebarOpen={setIsSidebarOpen}
+      />
+    <Box flexGrow={1}>
+      <Navbar
+       user = {data || {}}
+      isSidebarOpen={isSidebarOpen}
+      setIsSidebarOpen={setIsSidebarOpen}
+      
+      />
+      <Outlet />
+    </Box>
+  </Box>
+  );
+}
+
+export default Layout;
